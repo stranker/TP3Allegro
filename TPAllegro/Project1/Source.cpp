c3d48f6807e065922d5ddb62eb75290a6f526cd4
Unknown <dani_mrx93@hotmail.com>
2018-05-30 12:18:12 -0300

    Se agrega laser y su imagen

--- TPAllegro/Project1/Source.cpp
+++ TPAllegro/Project1/Source.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-enum KEYS{UP,DOWN,LEFT,RIGHT};
+enum KEYS { UP, DOWN, LEFT, RIGHT };
 
 
 bool AABB(const float x1, const float y1, const float w1, const float h1, const float x2, const float y2, const float w2, const float h2)
@@ -19,6 +19,7 @@ int main(int argc, char **argv) {
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_BITMAP  *player = NULL;
 	ALLEGRO_BITMAP *enemy = NULL;
+	ALLEGRO_BITMAP *laser = NULL;
 	ALLEGRO_TIMER* timer = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	int playerX = 200;
@@ -29,6 +30,10 @@ int main(int argc, char **argv) {
 	int enemyY = 400;
 	const int enemyW = 60;
 	const int enemyH = 96;
+	int laserX;
+	int laserY;
+	const int laserW = 40;
+	const int laserH = 8;
 	const int FPS = 60;
 	const int SCREEN_W = 800;
 	const int SCREEN_H = 600;
@@ -36,7 +41,8 @@ int main(int argc, char **argv) {
 	bool keys[4] = { false, false, false, false };
 	bool redraw = true;
 	bool gameOver = false;
-	
+	bool canShoot = true;
+
 
 	if (!al_init()) {
 		al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!",
@@ -51,7 +57,7 @@ int main(int argc, char **argv) {
 	}
 
 	display = al_create_display(SCREEN_W, SCREEN_H);
-	
+
 
 	if (!display) {
 		al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!",
@@ -67,20 +73,14 @@ int main(int argc, char **argv) {
 
 	player = al_load_bitmap("player.png");
 	enemy = al_load_bitmap("sal.png");
+	laser = al_load_bitmap("laser.png");
 	al_set_target_bitmap(player);
 	al_set_target_bitmap(enemy);
+	al_set_target_bitmap(laser);
 	al_set_target_backbuffer(display);
-	
-	
-	if (!player) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
-			NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		al_destroy_display(display);
-		return 0;
-	}
+
 	event_queue = al_create_event_queue();
 	al_install_keyboard();
-
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
@@ -99,6 +99,15 @@ int main(int argc, char **argv) {
 		}
 		if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
 			switch (ev.keyboard.keycode) {
+			case ALLEGRO_KEY_SPACE:
+				if (canShoot)
+				{
+					laserX = playerX + playerW;
+					laserY = playerY + 5;
+					canShoot = false;
+				}
+
+				break;
 			case ALLEGRO_KEY_UP:
 				keys[UP] = true;
 				break;
@@ -147,13 +156,7 @@ int main(int argc, char **argv) {
 			playerY = 0;
 		else if (playerY > SCREEN_H - playerH)
 			playerY = SCREEN_H - playerH;
-		if (redraw && al_is_event_queue_empty(event_queue)) {
-			redraw = false;
-			al_clear_to_color(al_map_rgb(50, 75, 0));
-			al_draw_bitmap(enemy, enemyX, enemyY, 0);
-			al_draw_bitmap(player, playerX, playerY, 0);
-			al_flip_display();
-		}
+
 		enemyX -= 2;
 		if (enemyX < -enemyW)
 		{
@@ -162,14 +165,28 @@ int main(int argc, char **argv) {
 		}
 		if (AABB(playerX, playerY, playerW, playerH, enemyX, enemyY, enemyW, enemyH))
 			gameOver = true;
+
+
+		if (redraw && al_is_event_queue_empty(event_queue)) {
+			redraw = false;
+			al_clear_to_color(al_map_rgb(50, 75, 0));
+			al_draw_bitmap(enemy, enemyX, enemyY, 0);
+			al_draw_bitmap(player, playerX, playerY, 0);
+			if (!canShoot)
+				al_draw_bitmap(laser, laserX, laserY, 0);
+			
+			al_flip_display();
+		}
+
 	}
 
-	
+
 	al_rest(0.1);
 
 	al_destroy_display(display);
 	al_destroy_bitmap(player);
 	al_destroy_bitmap(enemy);
+	al_destroy_bitmap(laser);
 	al_destroy_timer(timer);
 
 	return 0;
