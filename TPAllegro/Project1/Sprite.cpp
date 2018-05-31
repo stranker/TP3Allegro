610410cad75826d143cd5dd1a7f3c2e9b081f534
Unknown <dani_mrx93@hotmail.com>
2018-05-31 16:42:32 -0300

    Se crea clase Sprite de la cual heredan los demas objetos visibles

---
+++ TPAllegro/Project1/Sprite.cpp
@@ -0,0 +1,70 @@
+#include "Sprite.h"
+
+Sprite::Sprite(float posX, float posY, const char* fileLoc, int w, int h)
+{
+	sprite = al_load_bitmap(fileLoc);
+	positionX = posX;
+	positionY = posY;
+	spriteW = w;
+	spriteH = h;
+}
+
+Sprite::~Sprite()
+{
+	al_destroy_bitmap(sprite);
+}
+
+void Sprite::Draw()
+{
+	al_draw_bitmap(sprite, positionX, positionY, 0);
+}
+
+ALLEGRO_BITMAP * Sprite::GetSprite() const
+{
+	return sprite;
+}
+
+float Sprite::GetPosX() const
+{
+	return positionX;
+}
+
+float Sprite::GetPosY() const
+{
+	return positionY;
+}
+
+float Sprite::GetWidth() const
+{
+	return spriteW;
+}
+
+float Sprite::GetHeight() const
+{
+	return spriteH;
+}
+
+void Sprite::Move(float x, float y)
+{
+	positionX += x;
+	positionY += y;
+}
+
+void Sprite::SetPosition(float x, float y)
+{
+	positionX = x;
+	positionY = y;
+}
+
+void Sprite::Clamp(int xIni, int xFin, int yIni, int yFin)
+{
+	if (positionX < xIni)
+		positionX = xIni;
+	else if (positionX > xFin)
+		positionX = xFin;
+	if (positionY < yIni)
+		positionY = yIni;
+	else if (positionY > yFin)
+		positionY = yFin;
+}
+
---
+++ TPAllegro/Project1/Sprite.h
@@ -0,0 +1,36 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <iostream>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_native_dialog.h>
+
+using namespace std;
+
+class Sprite
+{
+private:
+	ALLEGRO_BITMAP * sprite = NULL;
+	float positionX;
+	float positionY;
+	int spriteW;
+	int spriteH;
+public:
+	Sprite(float posX, float posY, const char* fileLoc, int w, int h);
+	~Sprite();
+	float GetPosX() const;
+	float GetPosY() const;
+	float GetWidth() const;
+	float GetHeight() const;
+	ALLEGRO_BITMAP* GetSprite() const;
+	void Draw();
+	void Move(float x, float y);
+	void SetPosition(float x, float y);
+	void Clamp(int xIni, int xFin, int yIni, int yFin);
+};
+
+#endif
+
+
+
