2ecb19dd164250bc7fe4d9af0054c28385982e96
Unknown <dani_mrx93@hotmail.com>
2018-06-01 21:14:54 -0300

    Se agrega enemigo Tortuga

---
+++ TPAllegro/Project1/Tortuga.cpp
@@ -0,0 +1,57 @@
+#include "Tortuga.h"
+
+Tortuga::Tortuga(int SCREEN_W, int SCREEN_H) : Sprite(0, 0, "tortuga.png", 80, 40)
+{
+	Initialize(SCREEN_W, SCREEN_H);
+}
+
+Tortuga::Tortuga(float posX, float posY, const char * fileLoc, int w, int h) : Sprite(posX, posY, fileLoc, w , h)
+{
+}
+
+Tortuga::~Tortuga()
+{
+}
+
+void Tortuga::Movimiento(int SCREEN_W, int SCREEN_H)
+{
+	if (GetPosX() < 0 - GetWidth())
+	{
+		dir = -1;
+		SetPosition(0 - GetWidth(), rand() % SCREEN_H);
+		FlipH(true);
+	}
+	else if (GetPosX() > SCREEN_W)
+	{
+		dir = 1;
+		SetPosition(SCREEN_W, rand() % SCREEN_H);
+		FlipH(false);
+	}
+	Move(dir*speed, 0);
+}
+
+void Tortuga::Initialize(int SCREEN_W, int SCREEN_H)
+{
+	if (rand() % 100 > 50)
+	{
+		dir = 1;
+		SetPosition(0, rand() % SCREEN_H);
+		FlipH(true);
+	}
+	else
+	{
+		dir = -1;
+		SetPosition(SCREEN_W, rand() % SCREEN_H);
+		FlipH(false);
+	}
+}
+
+void Tortuga::Update(int SCREEN_W, int SCREEN_H)
+{
+	Movimiento(SCREEN_W, SCREEN_H);
+}
+
+void Tortuga::Kill(int SCREEN_W, int SCREEN_H)
+{
+	Initialize(SCREEN_W, SCREEN_H);
+}
---
+++ TPAllegro/Project1/Tortuga.h
@@ -0,0 +1,21 @@
+#ifndef TORTUGA_H
+#define TORTUTA_H
+
+#include "Sprite.h"
+
+class Tortuga :	public Sprite
+{
+private:
+	const int speed = 1;
+	int dir = 1;
+public:
+	Tortuga(int SCREEN_W, int SCREEN_H);
+	Tortuga(float posX, float posY, const char* fileLoc, int w, int h);
+	~Tortuga();
+	void Movimiento(int SCREEN_W, int SCREEN_H);
+	void Initialize(int SCREEN_W, int SCREEN_H);
+	void Update(int SCREEN_W, int SCREEN_H);
+	void Kill(int SCREEN_W, int SCREEN_H);
+};
+
+#endif
